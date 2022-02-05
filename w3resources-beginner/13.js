0b9ab2b864601dfeb3a91601fe30612407ed6c82
islomaliy <kislombek71.com>
2022-02-05 23:36:01 +0500

    first message

---
+++ w3resources-beginner/13.js
@@ -0,0 +1,11 @@
+// Write a JavaScript exercise to create a variable using a user-defined name.
+
+let name = 'example_name'
+
+let n = 120
+this[name] =  n;
+this.name =  // n; this also works fine
+
+console.log(name)
+
+// console.log('test');
---
+++ w3resources-beginner/14.js
@@ -0,0 +1,4 @@
+// Write a JavaScript exercise to get the extension of a filename.
+
+filename = "app.js"
+console.log(filename.split('.').pop());
